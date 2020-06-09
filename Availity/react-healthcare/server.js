c249a0a5b2832e1651e05ee59dd37cd3795b4ad8
Eric Downey <edowney29@gmail.com>
2020-06-09 15:00:21 -0400

    Update server.js

--- Availity/react-healthcare/server.js
+++ Availity/react-healthcare/server.js
@@ -4,9 +4,9 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
-    return res.send('pong');
-});
+// app.get('/ping', function (req, res) {
+//     return res.send('pong');
+// });
 
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
